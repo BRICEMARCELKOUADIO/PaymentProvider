cdad55c6ac46909e20279afe26a48a02e4a6e2f5
Brice Marcel <kbricemarcel@outlook.fr>
2021-02-22 12:33:53 +0000

    Ajoutez des fichiers projet.

---
+++ PaymentProvider.Domains/DataAggregate/Models/Paiement.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PaymentProvider.Domains.DataAggregate.Models
+{
+    public class Paiement
+    {
+        public string Amount { get; set; }
+        public string Currency { get; set; }
+        public string SiteID { get; set; }
+        public string ApiKey { get; set; }
+        public string TransactionId { get; set; }
+        public DateTime TansactionDate { get; set; }
+        public string PaymentConfiguration { get; set; }
+        public string PageAction { get; set; }
+        public string Language { get; set; }
+    }
+}
---
+++ PaymentProvider.Infrastructure/PaymentService/PaymentService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PaymentProvider.Infrastructure.PaymentService
+{
+    public class PaymentService
+    {
+    }
+}

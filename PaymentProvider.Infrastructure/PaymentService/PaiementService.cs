15551698ad3fff7826f902406f45489af41c910b
Brice Marcel <kbricemarcel@outlook.fr>
2021-03-09 19:30:35 +0000

    Correction

--- PaymentProvider.Infrastructure/PaymentService/PaiementService.cs
+++ PaymentProvider.Infrastructure/PaymentService/PaiementService.cs
@@ -29,19 +29,19 @@ namespace PaymentProvider.Infrastructure.PaymentService
             {
                 var client = new HttpClient();
 
-                var paiementDatet = new List<KeyValuePair<string, string>>();
+                var paiementData = new List<KeyValuePair<string, string>>();
 
-                paiementDatet.Add(new KeyValuePair<string, string>("cpm_amount", paiement.Amount.ToString()));
-                paiementDatet.Add(new KeyValuePair<string, string>("cpm_currency", _paiementOptions.Currency.ToString()));
-                paiementDatet.Add(new KeyValuePair<string, string>("apikey", _paiementOptions.ApiKey));
-                paiementDatet.Add(new KeyValuePair<string, string>("cpm_site_id", _paiementOptions.SiteID));
-                paiementDatet.Add(new KeyValuePair<string, string>("cpm_trans_id", paiement.TransactionId));
-                paiementDatet.Add(new KeyValuePair<string, string>("cpm_trans_date", paiement.TansactionDate.ToString()));
-                paiementDatet.Add(new KeyValuePair<string, string>("cpm_page_action", _paiementOptions.PageAction));
-                paiementDatet.Add(new KeyValuePair<string, string>("cpm_language", _paiementOptions.Language));
-                paiementDatet.Add(new KeyValuePair<string, string>("cpm_payment_config", _paiementOptions.PaymentConfiguration));
+                paiementData.Add(new KeyValuePair<string, string>("cpm_amount", paiement.Amount.ToString()));
+                paiementData.Add(new KeyValuePair<string, string>("cpm_currency", _paiementOptions.Currency.ToString()));
+                paiementData.Add(new KeyValuePair<string, string>("apikey", _paiementOptions.ApiKey));
+                paiementData.Add(new KeyValuePair<string, string>("cpm_site_id", _paiementOptions.SiteID));
+                paiementData.Add(new KeyValuePair<string, string>("cpm_trans_id", paiement.TransactionId));
+                paiementData.Add(new KeyValuePair<string, string>("cpm_trans_date", paiement.TansactionDate.ToString()));
+                paiementData.Add(new KeyValuePair<string, string>("cpm_page_action", _paiementOptions.PageAction));
+                paiementData.Add(new KeyValuePair<string, string>("cpm_language", _paiementOptions.Language));
+                paiementData.Add(new KeyValuePair<string, string>("cpm_payment_config", _paiementOptions.PaymentConfiguration));
 
-                var req = new HttpRequestMessage(HttpMethod.Post, _paiementOptions.SignatureUrl) { Content = new FormUrlEncodedContent(paiementDatet) };
+                var req = new HttpRequestMessage(HttpMethod.Post, _paiementOptions.SignatureUrl) { Content = new FormUrlEncodedContent(paiementData) };
 
                 var res = await client.SendAsync(req);
 
